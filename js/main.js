f1793b909d6c9f85e510c23ffad49d9c0d230c51
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-27 16:11:52 +0200

    Add exercise

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-new app = new Vue({
+const app = new Vue({
     el: "#root",
     data: {
-
+        emailArray: [],
+        arrayLength: 10
     },
     methods: {
 
     },
     mounted() {
-        
+        for( let i = 0; i < this.arrayLength; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emailArray.push(response.data.response);
+                })
+        }
     }
 })
